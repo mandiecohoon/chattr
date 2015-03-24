0860d8be533d471117e33c4a45e4d119975908f7
Amanda Cohoon <mandiecohoon@gmail.com>
2015-03-24 09:13:37 -0400

    Added getResults() and changed getCredentials() to public static.

--- src/java/chattr/ChattrService.java
+++ src/java/chattr/ChattrService.java
@@ -6,10 +6,56 @@
 
 package chattr;
 
+import java.io.StringWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.json.Json;
+import javax.json.stream.JsonGenerator;
+import javax.json.stream.JsonGeneratorFactory;
+import javax.ws.rs.Path;
+
 /**
  *
  * @author Amanda Cohoon - c0628569
  */
+
+@Path("/room")
 public class ChattrService {
-    
+    private String getResults(String query, String... params) {
+        String result = "";
+        
+        try (Connection conn = Credentials.getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            
+            for (int i = 1; i <= params.length; i++) {
+                pstmt.setString(i, params[i - 1]);
+            }
+            
+            ResultSet rs = pstmt.executeQuery();
+            StringWriter out = new StringWriter();
+            JsonGeneratorFactory factory = Json.createGeneratorFactory(null);
+            JsonGenerator gen = factory.createGenerator(out);
+            
+            gen.writeStartArray();
+            while (rs.next()) {
+                gen.writeStartObject()
+                    .write("productId", rs.getInt("productID"))
+                    .write("name", rs.getString("name"))
+                    .write("description", rs.getString("description"))
+                    .write("quantity", rs.getInt("quantity"))
+                    .writeEnd();
+            }
+            gen.writeEnd();
+            gen.close();
+            result = out.toString();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(ChattrService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
 }
