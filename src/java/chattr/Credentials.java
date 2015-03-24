9ecc5e032dfa7af84e5e6c72e1200bcb11c90a4f
Amanda Cohoon <mandiecohoon@gmail.com>
2015-03-24 09:06:24 -0400

    Added getConnection() to Credentials class

--- src/java/chattr/Credentials.java
+++ src/java/chattr/Credentials.java
@@ -6,6 +6,12 @@
 
 package chattr;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Amanda Cohoon - c0628569
@@ -13,5 +19,17 @@ package chattr;
 
 
 public class Credentials {
-    
+    private Connection getConnection() throws SQLException {
+        Connection conn = null;
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String jdbc = "jdbc:mysql://localhost/chattr";
+            conn = DriverManager.getConnection(jdbc, "root", ""); 
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Credentials.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return conn;
+    }
 }
